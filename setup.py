38080ae3ab510a874ffb6e3e5c4752e5685e82c8
diegoabt <dabtheuerkauf@gmail.com>
2023-07-17 22:19:53 +0200

    Modify `setup.py` for packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
-import os
+from setuptools import setup, find_packages
 
-os.system('pip3 install numpy==1.20.0')
-os.system('pip3 install networkx==2.5.1')
-os.system('pip3 install scipy==1.7.0')
-os.system('pip3 install scikit-image==0.18.3')
-os.system('pip3 install jupyter')
-os.system('pip3 install nbimporter')
-os.system('pip3 install pandas')
-os.system('pip3 install tqdm')
-os.system('pip3 install Pillow')
-os.system('pip3 install matplotlib')
-os.system('pip3 install scikit-umfpack')
+setup(
+    name='modi',
+    version='0.1.0',
+    description='MODI: Multicommodity Optimal Transport-based Dynamics for Image Classification',
+    author='Alessandro Lonardi',
+    author_email='alessandro.lonardi.vr@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'numpy==1.20.0',
+        'networkx==2.5.1',
+        'scipy==1.7.0',
+        'scikit-image==0.18.3',
+        'jupyter',
+        'nbimporter',
+        'pandas',
+        'tqdm',
+        'Pillow',
+        'matplotlib',
+        'scikit-umfpack'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
