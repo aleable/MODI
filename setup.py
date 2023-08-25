56edc249c82746abf952d87725bc7d0ff82532d1
diegoabt <dabtheuerkauf@gmail.com>
2023-08-25 11:02:13 +0200

    Add setup files for last pypi version (`0.1.5`)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='modi-flows',
-    version='0.1.3',
+    version='0.1.5',
     description='MODI: Multicommodity Optimal Transport-based Dynamics for Image Classification',
     author=['Alessandro Lonardi','Diego Baptista'],
     author_email='diego.theuerkauf@tuebingen.mpg.de',
